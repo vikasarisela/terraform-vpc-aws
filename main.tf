f772fdaddc69806b3358656b133fd71b13f9cd5b
unknown <vikas.arisela@gmail.com>
2025-11-02 17:10:14 +0400

    terraform modules

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "aws_vpc" "main" {
+  cidr_block       =  var.vpc_cidr
+  instance_tenancy = "default"
+  enable_dns_hostnames = true
+
+  tags = merge(
+    local.common_tags,
+    var.vpc_tags,
+    {
+        Name = local.common_name_suffix
+    }
+  )
+  }
+
+
---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "vpc_cidr" {
+    type = string
+  description = "please provide vpc cidr"
+}
+
+variable "project_name" {
+  type = "string"
+}
+variable "environment" {
+  type = string
+}
+
+variable "vpc_tags" {
+  type = map
+  default = {}   # it makes optional to provide vpc tags
+}
