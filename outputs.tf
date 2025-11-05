8a213c830811338d379ba6241749fb03d93099b1
unknown <vikas.arisela@gmail.com>
2025-11-05 22:16:38 +0400

    terraform basics

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,24 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
-output "aws_subnet" {
-  value = aws_subnet.public
-}
+# output "subn" {
+#   description = "public subnet testing................"
+#   value = aws_subnet.public[0].id
+# }
+
+
+# output "public_subnet_ids" {
+#   description = "IDs of all public subnets"
+#   value       = aws_subnet.public[*].id
+# }
+
+# output "test_message" {
+#   value = "Terraform output working"
+# output "public_subnet_ids" {
+#   description = "IDs of all public subnets"
+#   value       = module.vpc.aws_subnet.public[*].id
+# }
+
+
+
+# }
--- peering.tf
+++ peering.tf
@@ -1,24 +1,31 @@
-# resource "aws_vpc_peering_connection" "foo" { 
-#   count =  var.is_peering_required ? 1 : 0
+resource "aws_vpc_peering_connection" "default" { 
+  count =  var.is_peering_required ? 1 : 0
  
-#   peer_vpc_id   = data.aws_vpc.default.id  #acceptor  default
-#   vpc_id        = aws_vpc.main.id           # roboshop
+  peer_vpc_id   = data.aws_vpc.default.id  #acceptor  default
+  vpc_id        = aws_vpc.main.id           # roboshop
 
-#   accepter {
-#     allow_remote_vpc_dns_resolution = true
-#   }
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
 
-#   requester {
-#     allow_remote_vpc_dns_resolution = true
-#   }
+  requester {
+    allow_remote_vpc_dns_resolution = true
+  }
 
-#   auto_accept = true
+  auto_accept = true          # same account works
 
-#   tags = merge(
-#     local.common_tags,
-#     var.ig_tags,
-#     {
-#         Name = "${local.common_name_suffix}-default"
-#     }
-#   )
-# }
+  tags = merge(
+    local.common_tags,
+    var.ig_tags,
+    {
+        Name = "${local.common_name_suffix}-default"
+    }
+  )
+}
+
+resource "aws_route" "public_peering" {
+    count =  var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.public.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.default.id
+}
